d073087783017b18cf8e1ff5eedbc5aa811101b4
jieun <wldmsdlsns1@gmail.com>
2023-03-30 11:55:51 +0900

    기본 구조 완료

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
-        domains: ["dummyimage.com"],
+        domains: ["images.unsplash.com", "s3.us-west-2.amazonaws.com"],
     },
 };
 
